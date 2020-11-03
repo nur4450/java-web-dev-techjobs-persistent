34efd5e61a67765b33e17cc35a2133e5db8520ae
Nur Cetin <seymanur4450@gmail.com>
2020-11-03 15:36:58 -0600

    Created One-to-Many Relationship between job and employer.Finished Part 3.

--- queries.sql
+++ queries.sql
@@ -1,12 +1,12 @@
 ## Part 1: Test it with SQL
 SELECT column_name, data_type
 FROM information_schema.columns
-WHERE table_schema ='techjobs' AND table_name = 'job'
+WHERE table_schema ='techjobs' AND table_name = 'job';
 
 ## Part 2: Test it with SQL
 SELECT *
 FROM employer
 WHERE location = 'St. Louis City';
 ## Part 3: Test it with SQL
-
+DROP TABLE job;
 ## Part 4: Test it with SQL
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,13 +1,21 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
 
     @NotBlank(message = "Location cannot be blank.")
     private String location;
+
+    @OneToMany
+    @JoinColumn
+    private List<Job> jobs = new ArrayList<>();
 
     public Employer(String location){
         this.location = location;
